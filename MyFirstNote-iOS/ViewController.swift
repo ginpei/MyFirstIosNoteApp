4f2a88bce523ed143116bae7d32b5f2daa2fa0f6
ginpei <ginpei@gmail.com>
2017-06-01 15:53:48 -0700

    table view

--- MyFirstNote-iOS/ViewController.swift
+++ MyFirstNote-iOS/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var table: UITableView!
+    var data = ["Apple", "Banana", "C..."]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
